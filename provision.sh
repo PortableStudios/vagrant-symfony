4f842d23779830c759c36dbc92f55838007e9ab1
Jerram Watters <jerramw@gmail.com>
2014-05-25 22:10:07 +1000

    Get symfony running via NFS shares
    Add php.ini settings

--- provision.sh
+++ provision.sh
@@ -20,11 +20,30 @@ sudo add-apt-repository -y ppa:ondrej/php5-oldstable
 sudo apt-add-repository -y ppa:chris-lea/node.js
 
 sudo apt-get -y update && sudo apt-get -y dist-upgrade
-sudo apt-get -y install mcrypt acl libicu-dev php-pear php5-intl php5-dev fail2ban git drush php5-curl sendmail mysql-server php5-mysql phpmyadmin nodejs
-
+sudo apt-get -y install acl bindfs drush fail2ban git libicu-dev mcrypt mysql-server nodejs php5-curl php5-dev php5-intl php5-mysql phpmyadmin php-pear
+sudo hostname symfony.vm
+echo 'symfony.vm' | sudo tee -a /etc/hostname
 sudo a2enmod rewrite
 sudo npm install -g less
+
+#Php Configuration
+sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 10M/" /etc/php5/apache2/php.ini
+sed -i "s/short_open_tag = On/short_open_tag = Off/" /etc/php5/apache2/php.ini
+sed -i "s/;date.timezone =/date.timezone = UTC/" /etc/php5/apache2/php.ini
+sed -i "s/;date.timezone =/date.timezone = UTC/" /etc/php5/cli/php.ini
+sed -i "s/memory_limit = 128M/memory_limit = 1024M/" /etc/php5/apache2/php.ini
+sed -i "s/_errors = Off/_errors = On/" /etc/php5/apache2/php.ini
+#sudo bindfs -o perms=0775,mirror=vagrant,group=www-data /home/vagrant/www /var/www
 sudo service apache2 restart
+
+# Installing composer globally
+sudo curl -s https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+sudo chmod +x /usr/local/bin/composer
+# add to ~/.bash_aliases
+echo "alias composer='/usr/local/bin/composer'" >> /home/vagrant/.bash_aliases
+# reload
+. /home/vagrant/.bash_aliases
 
 #is this needed? already installed with apt-get above
 #sudo pear install pecl/intl
@@ -32,3 +51,4 @@ sudo service apache2 restart
 ## other tasks
 # SSL / HTTPS
 # timezone to UTC
+# hostname
--- symfony-install.sh
+++ symfony-install.sh
@@ -1,28 +1,19 @@
 ####################################
-# Installing composer globally
-sudo curl -s https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
-# add to ~/.bash_aliases
-echo "alias composer='/usr/local/bin/composer'" >> /home/`whoami`/.bash_aliases
-# reload
-. /home/`whoami`/.bash_aliases
-
 # install Symfony via composer
-sudo mkdir /var/vhosts && sudo chown `whoami`:www-data /var/vhosts
-composer create-project --prefer-dist -vvv symfony/framework-standard-edition symfony 2.4.*
+# deploying to /tmp, and settings permissions works after you move it to /var/vhosts
+# but on linux at least I have it working in place via nfs and bindfs
+# So im deploying in place
+#composer create-project --prefer-dist -vvv symfony/framework-standard-edition /tmp/symfony 2.4.*
+composer create-project --prefer-dist -vvv symfony/framework-standard-edition /var/vhosts/symfony 2.4.*
+#cd /tmp/symfony
 cd /var/vhosts/symfony
 sudo chown `whoami`:www-data -R ./ && sudo find . -type f -exec chmod 664 {} \; && sudo find . -type d -exec chmod 775 {} \;
 sudo chmod +x app/console
 sudo rm -rf app/cache/* app/logs/*
 sudo setfacl -R -m u:www-data:rwX -m u:`whoami`:rwX app/cache app/logs
 sudo setfacl -dR -m u:www-data:rwX -m u:`whoami`:rwX app/cache app/logs
-sudo rm -rf /var/www
-sudo ln -s /var/vhosts/symfony/web /var/www
-# this will get you going
-# you cant symlink the code into a host accissible location -
-# the symlink will try to reference the host file system not the guest file system
-#sudo ln -s /var/vhosts/symfony /vagrant/symfony
-cd /var/vhosts/symfony
+#mv /tmp/symfony /var/vhosts
+#cd /var/vhosts/symfony
 
 ## todo
 # add default install to git and replace all of this
